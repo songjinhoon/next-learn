7ba2a58db369ec948f9785c6d5afaed12743f263
songjinhoon <jinhoon0319@gmail.com>
2023-11-23 16:39:47 +0900

    font 제거

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import '@/app/ui/global.css';
-import { inter } from '@/app/ui/fonts';
+
 import React, { ReactNode } from 'react';
 
 export default function RootLayout({ children }: { children: ReactNode; }) {
   return (
     <html lang='en'>
-    <body className={`${inter.className}`}>{children}</body>
+    <body>{children}</body>
     </html>
   );
 }
